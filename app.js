9154b9bd64e8cac086ee851893bf95d48f075ac2
simoneputeo <s.puteo@gmail.com>
2025-05-13 15:00:57 +0200

    various bugs to fix

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const postRouter = require('./routers/posts.js')
+
+
+app.use(postRouter);
+
+app.listen(port, () => {
+  console.log(`Il server è in ascolto sulla porta ${port}`)
+})
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.json({message: 'Server del mio blog'})
+});
+
+
+
